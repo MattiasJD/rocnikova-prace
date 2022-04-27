625d2bbf3714b235d970e57a0be6fe4dccb18ba0
MattiasJD <maty.demjan@seznam.cz>
2022-04-28 01:21:34 +0200

    lettsgooooo

--- src/main/java/com/example/csmore/creditPhoneController.java
+++ src/main/java/com/example/csmore/creditPhoneController.java
@@ -5,11 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class creditPhoneController {
     public Label infoText;
+    public ImageView phoneField;
+    public TextField amountField;
+    public ChoiceBox comboPicker;
 
     public void buttonSevenClick(ActionEvent actionEvent) {
     }
